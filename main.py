132ce50589f7244011f536d67cc24477dadbf67b
OGrocketa <jaroslaw.sedach@gmail.com>
2025-04-02 20:08:00 +0200

    wip

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ import gradio as gr
 
 with gr.Blocks() as demo:
     gr.Markdown("# CrewAI Demo")
-    userInput = gr.Textbox(lines=5, label="Enter your query")
-    output = gr.Textbox(lines=5, label="Output Box")
-    submitBtn = gr.Button("Submit")
+    with gr.Row():
+        with gr.Column(scale=1):
+            gr.Dropdown(choices=['DataSummarizer','Some other agent'], label="Select Agent",interactive=True)
+            userInput = gr.Textbox(lines=5, label="Enter your query")
+            submitBtn = gr.Button("Submit")
+    
+        with gr.Column(scale=2):
+            processLogs = gr.Textbox(lines=5, label="Logs", autoscroll=True, interactive=False)
+            output = gr.Textbox(lines=5, label="Output Box")
 
     @submitBtn.click(inputs=userInput, outputs=output)
     def get_answer(userInput):
